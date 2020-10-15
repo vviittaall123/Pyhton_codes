97dfde89c3198d87db4e26a96c61005fc81653d0
jhonash740 <72943327+jhonash740@users.noreply.github.com>
2020-10-16 00:25:56 +0530

    Update ASCII.py

--- ASCII.py
+++ ASCII.py
@@ -1,4 +1,5 @@
 # Program to find the ASCII value of the given character
 
 c = 'p'
-print("The ASCII value of '" + c + "' is", ord(c))
+print("The ASCII value of '" + c + "' is", sum(c))
+# I have changed it to the sum function to ittirate the value
