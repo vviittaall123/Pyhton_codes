59a87af73ae0decf4e7a0e66cda0db757ac959ff
vviittaall123 <71178421+vviittaall123@users.noreply.github.com>
2021-09-30 22:34:02 +0530

    Update ASCII.py
    printing pattern and loop and print ascii of character

--- ASCII.py
+++ ASCII.py
@@ -2,3 +2,20 @@
 
 c = 'p'
 print("The ASCII value of '" + c + "' is", ord(c))
+
+
+
+string = 'djbwehkwchwdvcwcvhj'
+for i in string :
+  print("The ASCII value of '" + i + "' is", ord(i))
+  
+  
+  
+for i in range(0, 5):
+  for j in range(0, i+1):
+         
+    # printing stars
+    print("* ",end="")
+      
+    # ending line after each row
+    print("\r")
