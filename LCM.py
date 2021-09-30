366bbc2cd6c036a5e292725ddefe7a5f82bca58f
vviittaall123 <71178421+vviittaall123@users.noreply.github.com>
2021-09-30 22:37:49 +0530

    Update LCM.py
    lcm by recursion added

--- LCM.py
+++ LCM.py
@@ -17,3 +17,21 @@ num1 = 54
 num2 = 24
 
 print("The L.C.M. is", compute_lcm(num1, num2))
+
+
+# lcm by recursion
+def lcm(a,b):
+    lcm.multiple=lcm.multiple+b
+    if((lcm.multiple % a == 0) and (lcm.multiple % b == 0)):
+        return lcm.multiple;
+    else:
+        lcm(a, b)
+    return lcm.multiple
+lcm.multiple=0
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+if(a>b):
+    LCM=lcm(b,a)
+else:
+    LCM=lcm(a,b)
+print(LCM)
